de5d4c27e76be108e9272a7df6ded77b1bf5f6fe
Myung Cheol Kim <bluevoice27@gmail.com>
2015-03-19 15:23:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import sys
 from function import *
 
+# 파싱할 url
 parseUrlLinks = ['http://media.daum.net/economic/']
 
 
@@ -16,9 +17,11 @@ while i < urlCounts:
   if addUrlLinks == '' :  
     i = i + 1
     continue
-
+  
+  # text를 파싱할 주소를 rabbitmq에 전달.
   sendUrlToMq(parseUrlLinks[i])
-
+  
+  # url문서 내부에 외부 링크 url이 있을 경우 루프를 돌고 있는 해당 url list에 추가함.
   parseUrlLinks = list(set(parseUrlLinks + addUrlLinks))
 
   urlCounts = len(parseUrlLinks)
@@ -28,6 +31,7 @@ while i < urlCounts:
   #if i == 5 : break
 
 
+# 링크가 정상적으로 추출됬는지 확인을 위해... 추후 삭제 해야...
 f = open('links', 'w')
 for url in parseUrlLinks :
   f.write(url + '\n')
